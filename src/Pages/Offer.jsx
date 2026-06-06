065ec4d3c517f1a2f9b4659548dd057eb2da05b1
Romain <romain@mbprom1.home>
2026-06-07 00:59:35 +0200

    clog debub

--- src/Pages/Offer.jsx
+++ src/Pages/Offer.jsx
@@ -32,7 +32,7 @@ const Offer = () => {
       const response = await axios.get(
         `${import.meta.env.VITE_API_URL}/offers`,
       );
-      console.log(response.data);
+      console.log(`response => ${response.data}`);
       const found = response.data.offers.find((item) => item._id === id);
       setOffer(found);
       setIsLoading(false);
